7d31d08094456669b52d84def516cc4891a36661
YangGuoPeng <yangguopeng@ttg.cn>
2025-05-27 10:56:26 +0800

    底部添加ICP备案号

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,8 @@ import styles from './footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.container}>
-        Copyright © 2024 Magnolia的个人博客 All Rights Reserved
+      <p>粤ICP备2025417835号-1</p>
+      <p>Copyright © 2024 Magnolia的个人博客 All Rights Reserved</p>
     </div>
   )
 }
