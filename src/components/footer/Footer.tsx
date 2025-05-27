d30ba85c55a1c59580ce1f513c9f426492969cc6
YangGuoPeng <yangguopeng@ttg.cn>
2025-05-27 11:34:48 +0800

    修改

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <p>粤ICP备2025417835号-1</p>
-      <p>Copyright © 2024 Magnolia的个人博客 All Rights Reserved</p>
+      <p>Copyright © 2025 Magnolia的个人博客 All Rights Reserved</p>
     </div>
   )
 }
