df539642877e374e153d7f042ec6334256b4c3c7
YangGuoPeng <yangguopeng@ttg.cn>
2025-06-06 14:11:04 +0800

    底部文字调整

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,17 +4,21 @@ import pic from '@/assets/image/police.png'
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <p>
-          <a href="https://beian.miit.gov.cn/#/Integrated/index" rel="noreferrer" target="_blank">
-            粤ICP备2025417835号-1
-          </a>
+      <div className={styles.beian_info}>
+        <p>
+            <a href="https://beian.miit.gov.cn/#/Integrated/index" rel="noreferrer" target="_blank">
+              粤ICP备2025417835号-1
+            </a>
         </p>
-      <p>
-        <img style={{width: '16px',
-            verticalAlign: 'text-top',
-            marginRight: '5px'}} src={pic.src} alt="公安备案图标" />
-        <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44030002007050" rel="noreferrer" target="_blank">粤公网安备44030002007050号</a>
-      </p>
+        <span style={{margin: '0 20px'}}>|</span>
+        <p>
+          <img style={{width: '16px',
+              verticalAlign: 'text-top',
+              marginRight: '5px'}} src={pic.src} alt="公安备案图标" />
+          <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44030002007050" rel="noreferrer" target="_blank">粤公网安备44030002007050号</a>
+        </p>
+      </div>
+      
       <p>Copyright © 2025 Magnolia的个人博客 All Rights Reserved</p>
     </div>
   )
